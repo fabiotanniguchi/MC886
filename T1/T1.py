670274ebc7b54a4efa354260354044f7501cda9f
Felipe Caminada <fcaminada@ciandt.com>
2017-08-31 23:27:26 -0300

    final

--- T1/T1.py
+++ T1/T1.py
@@ -5,10 +5,12 @@ asdasdasdas
 
 from sklearn import linear_model
 from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import cross_val_predict
+from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 import numpy as np
 import pandas as pd
@@ -41,21 +43,14 @@ def load_model_data(test_file = TRAINING_MODEL_FILE):
                         delimiter=',', skiprows=0)
 
     df = pd.DataFrame(data, columns=[
-                              'timbre01',
-                              'timbre02',
-                              'timbre04',
-                              'timbre06',
-                              'timbrec3',
-                              'timbrec8',
-                              'timbrec9',
-                              'timbrec10',
-                              'timbrec13',
-                              'timbrec23',
-                              'timbrec24',
-                              'timbrec73',
-                              'timbrec76',
-                              'timbrec77',
-                              'timbrec78',
+                              'timbre01', 'timbre02', 'timbre03', 'timbre04', 'timbre05', 'timbre06', 'timbre07', 'timbre08', 'timbre09', 'timbre10', 'timbre11', 'timbre12',
+                                          'timbrec1','timbrec2','timbrec3','timbrec4','timbrec5','timbrec6','timbrec7','timbrec8','timbrec9','timbrec10','timbrec11','timbrec12',
+                                          'timbrec13','timbrec14','timbrec15','timbrec16','timbrec17','timbrec18','timbrec19','timbrec20','timbrec21','timbrec22','timbrec23','timbrec24',
+                                          'timbrec25','timbrec26','timbrec27','timbrec28','timbrec29','timbrec30','timbrec31','timbrec32','timbrec33','timbrec34','timbrec35','timbrec36',
+                                          'timbrec37','timbrec38','timbrec39','timbrec40','timbrec41','timbrec42','timbrec43','timbrec44','timbrec45','timbrec46','timbrec47','timbrec48',
+                                          'timbrec49','timbrec50','timbrec51','timbrec52','timbrec53','timbrec54','timbrec55','timbrec56','timbrec57','timbrec58','timbrec59','timbrec60',
+                                          'timbrec61','timbrec62','timbrec63','timbrec64','timbrec65','timbrec66','timbrec67','timbrec68','timbrec69','timbrec70','timbrec71','timbrec72',
+                                          'timbrec73','timbrec74','timbrec75','timbrec76','timbrec77','timbrec78'
                             ]).abs()
     target = pd.DataFrame(data, columns=["year"])
     scaler = preprocessing.StandardScaler(with_mean=False).fit(df)
@@ -70,7 +65,9 @@ def load_training_testing_data(X, Y):
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=0, shuffle=True)
     if (True == USE_TEST_FILE):
         # Load the test file instead.
-        x_test, y_test = load_model_data(TESTING_MODEL_FILE)
+        df, target, x_test, y_test = load_model_data(TESTING_MODEL_FILE)
+        x_train = X
+        y_train = Y
     return x_train, x_test, y_train, y_test
 
 
@@ -110,19 +107,109 @@ def predict_linear_model(x_train, y_train, x_test, y_test):
     print('Variance score: %.2f' % r2_score(y_test, y_pred))
     print("Linear model:", pretty_print_linear(model.coef_))
 
-    #fig, ax = plt.subplots()
-    #ax.scatter(y_test, y_pred, edgecolors = (0, 0, 0), color = 'red')
-    #x.plot([1920, 2020], [1920, 2020], 'k--', lw = 4)
-    #ax.set_xlabel('Measured')
-    #ax.set_ylabel('Predicted')
-    #plt.show()
+    fig, ax = plt.subplots()
+    ax.scatter(y_test, y_pred, edgecolors = (0, 0, 0), color = 'red')
+    ax.plot([1920, 2020], [1920, 2020], 'k--', lw = 4)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plt.show()
+
+    plt.figure()
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+
+    # train_sizes, train_scores, test_scores = learning_curve(
+    #   model, x_train, y_train, train_sizes=[0.2, 0.5, 0.7], cv=4)
+    # train_scores_mean = np.mean(train_scores, axis=1)
+    # train_scores_std = np.std(train_scores, axis=1)
+    # test_scores_mean = np.mean(test_scores, axis=1)
+    # test_scores_std = np.std(test_scores, axis=1)
+
+    # plt.grid()
+    # plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+    #                  train_scores_mean + train_scores_std, alpha=0.1,
+    #                  color="r")
+    # plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+    #                  test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    # plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+    #          label="Training score")
+    # plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+    #          label="Cross-validation score")
+
+    # plt.legend(loc="best")
+    # plt.show()
+
+
+def predict_sgd_model(x_train, y_train, x_test, y_test):
+    iterations = [500]
+    score = [0, 0, 0, 0, 0]
+    for index, ite in enumerate(iterations):
+        sgd = linear_model.SGDRegressor(alpha=0.0001, average=False, epsilon=0.01, eta0=0.001,
+           fit_intercept=True, l1_ratio=0.15, learning_rate='invscaling',
+           loss='squared_loss', max_iter=ite, penalty='l2',
+           power_t=0.35, random_state=None, shuffle=True, tol=None,
+           verbose=0, warm_start=False)
+        model = sgd.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+
+        print("Mean squared error: %.2f"
+              % mean_squared_error(y_test, y_pred))
+        print('Variance score: %.2f' % r2_score(y_test, y_pred))
+        print("Linear model:", pretty_print_linear(model.coef_))
+        score[index] = r2_score(y_test, y_pred)
+        train_sizes, train_scores, test_scores = learning_curve(
+          model, x_train, y_train, train_sizes=[0.2, 0.5, 0.7], cv=4)
+        train_scores_mean = np.mean(train_scores, axis=1)
+        train_scores_std = np.std(train_scores, axis=1)
+        test_scores_mean = np.mean(test_scores, axis=1)
+        test_scores_std = np.std(test_scores, axis=1)
+
+        plt.grid()
+        plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+        plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1, color="g")
+        plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+        plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+
+        plt.legend(loc="best")
+        plt.show()
+
+    # fig, ax = plt.subplots()
+    # plt.plot(iterations, score, 'o-', color="r",
+    #          label="Training score")
+    # plt.xlabel("Iterations")
+    # plt.ylabel("Score")
+    # plt.show()
+
+
+
+def predict_linear_model_polinomial(x_train, y_train, x_test, y_test):
+    lm = linear_model.LinearRegression()
+    model = make_pipeline(PolynomialFeatures(), lm)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    print("Mean squared error: %.2f"
+          % mean_squared_error(y_test, y_pred))
+    print('Variance score: %.2f' % r2_score(y_test, y_pred))
+
+    fig, ax = plt.subplots()
+    ax.scatter(y_test, y_pred, edgecolors = (0, 0, 0), color = 'red')
+    ax.plot([1920, 2020], [1920, 2020], 'k--', lw = 4)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plt.show()
 
     plt.figure()
     plt.xlabel("Training examples")
     plt.ylabel("Score")
 
     train_sizes, train_scores, test_scores = learning_curve(
-      lm, x_train, y_train, train_sizes=[0.2, 0.5, 0.7], cv=4)
+      model, x_train, y_train, train_sizes=[0.2, 0.5, 0.7, 1], cv=4)
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
@@ -143,32 +230,6 @@ def predict_linear_model(x_train, y_train, x_test, y_test):
     plt.show()
 
 
-def predict_sgd_model(x_train, y_train, x_test, y_test):
-    iterations = [5, 10, 50, 100]
-    score = [0, 0, 0, 0]
-    for index, ite in enumerate(iterations):
-        sgd = linear_model.SGDRegressor(alpha=0.0001, average=False, epsilon=0.1, eta0=0.01,
-           fit_intercept=True, l1_ratio=0.15, learning_rate='invscaling',
-           loss='squared_loss', max_iter=ite, penalty='l2',
-           power_t=0.25, random_state=None, shuffle=True, tol=None,
-           verbose=0, warm_start=False)
-        model = sgd.fit(x_train, y_train)
-        y_pred = model.predict(x_test)
-
-        print("Mean squared error: %.2f"
-              % mean_squared_error(y_test, y_pred))
-        print('Variance score: %.2f' % r2_score(y_test, y_pred))
-        print("Linear model:", pretty_print_linear(model.coef_))
-        score[index] = r2_score(y_test, y_pred)
-
-    fig, ax = plt.subplots()
-    plt.plot(iterations, score, 'o-', color="r",
-             label="Training score")
-    plt.xlabel("Iterations")
-    plt.ylabel("Score")
-    plt.show()
-
-
 ###
 # Main function, executes the model prediction.
 ###
@@ -177,8 +238,9 @@ def main():
     x_train, x_test, y_train, y_test = load_training_testing_data(X, Y)
 
     #plot_model_values(x_test, y_test)
-    predict_linear_model(x_train, y_train, x_test, y_test)
-    #predict_sgd_model(x_train, y_train, x_test, y_test)
+    #predict_linear_model(x_train, y_train, x_test, y_test)
+    predict_sgd_model(x_train, y_train, x_test, y_test)
+    #predict_linear_model_polinomial(x_train, y_train, x_test, y_test)
 
 
 ###
